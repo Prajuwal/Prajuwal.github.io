deda1294de35a910b877d97cfa34e6f61742e583
Prajuwal <prajuwalsingh@gmail.com>
2023-10-14 19:10:25 +0530

    added links to projects

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -7,7 +7,7 @@ export const ProjectList = [
     image: proj,
     skills: "React,Redux,Typescript,TailwindCSS,Firebase,React Router",
     features: `Add and delete from cart,checkout,authentication`,
-    github:'',
+    github: "https://github.com/Prajuwal/tesla-Knockoff-Ecom",
   },
   {
     id: 1,
@@ -15,6 +15,7 @@ export const ProjectList = [
     image: proj,
     skills: "React,TailwindCSS,HTML,Javascript",
     features: `Add book,Edit book,Sorting,Filtering,Search`,
+    github: "https://github.com/Prajuwal/book-explorer",
   },
   {
     id: 2,
@@ -22,5 +23,6 @@ export const ProjectList = [
     image: proj,
     skills: "React,Redux,TailwindCSS,React Router",
     features: "Youtube Api,Search,auto-suggestions,comments,video play",
+    github: "https://github.com/Prajuwal/ourtube",
   },
 ];
--- src/pages/ProjectDisplay.jsx
+++ src/pages/ProjectDisplay.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import { ProjectList } from "../helpers/ProjectList";
 import { GitHub } from "@mui/icons-material";
 import "../styles/ProjectDisplay.css";
+import { Link } from "@mui/material";
 
 function ProjectDisplay({ id }) {
-  
-  console.log("id ye rhi"+ id)
   const project = ProjectList[id];
-
 
   return (
     <div className="project">
       <h1> {project.name}</h1>
       <img src={project.image} alt={project.name} />{" "}
-     
       <p>
         <b>Skills:</b> {project.skills}
       </p>
       <p>
         <b>Features:</b> {project.features}
       </p>
-      <GitHub />
+      <Link to={project.github}>
+        <GitHub />
+      </Link>
     </div>
   );
 }
